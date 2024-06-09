71f03e0765f1d7566c727f99dc7041a5fd714289
Brian Caldera <briancaldera07@gmail.com>
2024-06-09 19:55:42 -0400

    Added options to funcionario dashboard

--- resources/views/funcionario/dashboard.blade.php
+++ resources/views/funcionario/dashboard.blade.php
@@ -15,6 +15,21 @@
         </div>
     </div>
 
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        
+        <div class="grid grid-cols-4 gap-4">
+            <div class="col-span-full">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <h2 class="p-6 text-gray-900 dark:text-gray-100 text-xl font-extrabold">Menu</h2>
+                </div>
+            </div>
+            <a href=""><div class="rounded bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"><p class="p-6 text-gray-900 dark:text-gray-100">Eventos</p></div></a>
+            <a href=""><div class="rounded bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"><p class="p-6 text-gray-900 dark:text-gray-100">Opcion 2</p></div></a>
+            <a href=""><div class="rounded bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"><p class="p-6 text-gray-900 dark:text-gray-100">Opcion 3</p></div></a>
+            <a href=""><div class="rounded bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"><p class="p-6 text-gray-900 dark:text-gray-100">Opcion 4</p></div></a>
+        </div>
+    </div>
+
     <div>
         @if (Auth::user()->perfil->tipo == 'solicitante')
             <ul>
